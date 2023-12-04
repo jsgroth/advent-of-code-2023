4cbf44a5d676104442ee0422169282ddba67372b
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-12-03 23:36:45 -0600

    simpler day 4 solution

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -5,54 +5,39 @@
 use std::cmp;
 use std::error::Error;
 
-#[derive(Debug, Clone)]
-struct Card {
-    winning_numbers: Vec<u32>,
-    your_numbers: Vec<u32>,
+fn solve_part_1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let win_count = count_winning_numbers(line);
+            if win_count != 0 { 2_u32.pow(win_count - 1) } else { 0 }
+        })
+        .sum()
 }
 
-fn parse_input(input: &str) -> impl Iterator<Item = Card> + '_ {
-    input.lines().map(|line| {
-        let (_, numbers_str) = line.split_once(": ").expect("Invalid line");
-        let (winning_numbers, your_numbers) = numbers_str.split_once(" | ").expect("Invalid line");
+fn count_winning_numbers(line: &str) -> u32 {
+    let (_, numbers_str) = line.split_once(": ").expect("Invalid line");
+    let (winning_numbers, your_numbers) = numbers_str.split_once(" | ").expect("Invalid line");
 
-        let winning_numbers = parse_numbers(winning_numbers);
-        let your_numbers = parse_numbers(your_numbers);
+    let winning_numbers = parse_numbers(winning_numbers);
+    let your_numbers = parse_numbers(your_numbers);
 
-        Card { winning_numbers, your_numbers }
-    })
+    your_numbers.into_iter().filter(|number| winning_numbers.contains(number)).count() as u32
 }
 
 fn parse_numbers(numbers: &str) -> Vec<u32> {
     numbers.split(' ').filter_map(|s| s.parse::<u32>().ok()).collect()
 }
 
-fn solve_part_1(input: &str) -> u32 {
-    parse_input(input)
-        .map(|card| {
-            let count = count_winning_numbers(&card);
-            if count != 0 { 2_u32.pow(count - 1) } else { 0 }
-        })
-        .sum()
-}
-
-fn count_winning_numbers(card: &Card) -> u32 {
-    card.your_numbers.iter().filter(|number| card.winning_numbers.contains(number)).count() as u32
-}
-
 fn solve_part_2(input: &str) -> u32 {
-    let cards: Vec<_> = parse_input(input).collect();
+    let num_cards = input.lines().count();
 
-    // Start with 1 of every card
-    let mut card_counts = vec![1; cards.len()];
+    let mut card_counts = vec![1; num_cards];
 
-    for (i, card) in cards.iter().enumerate() {
-        let win_count = count_winning_numbers(card);
-        if win_count == 0 {
-            continue;
-        }
+    for (i, line) in input.lines().enumerate() {
+        let win_count = count_winning_numbers(line);
 
-        let end = cmp::min(i + win_count as usize + 1, cards.len());
+        let end = cmp::min(i + win_count as usize + 1, card_counts.len());
         for j in i + 1..end {
             card_counts[j] += card_counts[i];
         }
