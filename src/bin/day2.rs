2e13dadaa2eb13a77af07fd784e78fef3bbd2290
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-12-06 21:05:50 -0600

    experiment with using nom for input parsing

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -2,41 +2,19 @@
 //!
 //! <https://adventofcode.com/2023/day/2>
 
+use nom::bytes::complete::{tag, take_while1};
+use nom::character::complete::char;
+use nom::combinator::map_res;
+use nom::multi::separated_list1;
+use nom::sequence::{delimited, separated_pair};
+use nom::IResult;
 use std::error::Error;
-use std::str::FromStr;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct Reveal {
     red: Option<u32>,
     green: Option<u32>,
     blue: Option<u32>,
-}
-
-impl FromStr for Reveal {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let err_fn_0 = || format!("Invalid reveal string: '{s}'");
-        let err_fn_1 = |err| format!("Invalid reveal string: '{s}': {err}");
-
-        let mut red = None;
-        let mut green = None;
-        let mut blue = None;
-
-        for cubes_str in s.split(", ") {
-            let (num_str, color) = cubes_str.split_once(' ').ok_or_else(err_fn_0)?;
-            let num = num_str.parse().map_err(err_fn_1)?;
-
-            match color {
-                "red" => red = Some(num),
-                "green" => green = Some(num),
-                "blue" => blue = Some(num),
-                _ => return Err(err_fn_0()),
-            }
-        }
-
-        Ok(Self { red, green, blue })
-    }
 }
 
 #[derive(Debug, Clone)]
@@ -45,31 +23,43 @@ struct Game {
     reveals: Vec<Reveal>,
 }
 
-impl FromStr for Game {
-    type Err = String;
+fn parse_u32(input: &str) -> IResult<&str, u32> {
+    map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| s.parse::<u32>())(input)
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let err_fn_0 = || format!("Invalid game string: '{s}'");
-        let err_fn_1 = |err| format!("Invalid game string: '{s}': {err}");
+fn parse_reveal_field(input: &str) -> IResult<&str, (u32, &str)> {
+    separated_pair(parse_u32, char(' '), take_while1(char::is_alphabetic))(input)
+}
 
-        let (game_str, reveals_str) = s.split_once(": ").ok_or_else(err_fn_0)?;
-        let (_, game_id_str) = game_str.split_once(' ').ok_or_else(err_fn_0)?;
-        let game_id = game_id_str.parse().map_err(err_fn_1)?;
+fn parse_reveal(input: &str) -> IResult<&str, Reveal> {
+    let (rest, fields) = separated_list1(tag(", "), parse_reveal_field)(input)?;
 
-        let reveals = reveals_str
-            .split("; ")
-            .map(|reveal_str| reveal_str.parse::<Reveal>())
-            .collect::<Result<_, _>>()?;
+    let reveal = fields.into_iter().fold(Reveal::default(), |mut reveal, (number, color)| {
+        match color {
+            "red" => reveal.red = Some(number),
+            "green" => reveal.green = Some(number),
+            "blue" => reveal.blue = Some(number),
+            _ => panic!("Invalid color string: {color}"),
+        }
+        reveal
+    });
 
-        Ok(Self { id: game_id, reveals })
-    }
+    Ok((rest, reveal))
+}
+
+fn parse_game(input: &str) -> IResult<&str, Game> {
+    let (rest, game_id) = delimited(tag("Game "), parse_u32, tag(": "))(input)?;
+
+    let (rest, reveals) = separated_list1(tag("; "), parse_reveal)(rest)?;
+
+    Ok((rest, Game { id: game_id, reveals }))
 }
 
 fn solve_part_1(input: &str) -> u32 {
     input
         .lines()
         .filter_map(|line| {
-            let game = line.parse::<Game>().expect("Failed to parse game line");
+            let (_, game) = parse_game(line).expect("Failed to parse game");
 
             game.reveals
                 .iter()
@@ -87,7 +77,7 @@ fn solve_part_2(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            let game = line.parse::<Game>().expect("Failed to parse game line");
+            let (_, game) = parse_game(line).expect("Failed to parse game");
 
             let red = game.reveals.iter().filter_map(|reveal| reveal.red).max().unwrap_or(0);
             let green = game.reveals.iter().filter_map(|reveal| reveal.green).max().unwrap_or(0);
